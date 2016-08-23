8cdf6b88198419834de3bbf04bb76f2bf6575158
Sander Tuin <git@sander.garden>
2016-08-23 02:12:27 +0200

    Minor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,19 @@
 function toggleClassMenu() {
-	myMenu.classList.add("menu--animatable");
-	if(!myMenu.classList.contains("menu--visible")) {
-		myMenu.classList.add("menu--visible");
+	myMenu.classList.add('menu--animatable');
+	if(!myMenu.classList.contains('menu--visible')) {
+		myMenu.classList.add('menu--visible');
 	} else {
 		myMenu.classList.remove('menu--visible');
 	}
 }
 
 function OnTransitionEnd() {
-	myMenu.classList.remove("menu--animatable");
+	myMenu.classList.remove('menu--animatable');
 }
 
-var myMenu = document.querySelector(".menu");
-var oppMenu = document.querySelector(".menu-icon");
-var closeMenu = document.querySelector(".menu-close");
-myMenu.addEventListener("transitionend", OnTransitionEnd, false);
-oppMenu.addEventListener("click", toggleClassMenu, false);
-closeMenu.addEventListener("click", toggleClassMenu, false);
+var myMenu = document.querySelector('.menu');
+var oppMenu = document.querySelector('.menu-icon');
+var closeMenu = document.querySelector('.menu-close');
+myMenu.addEventListener('transitionend', OnTransitionEnd, false);
+oppMenu.addEventListener('click', toggleClassMenu, false);
+closeMenu.addEventListener('click', toggleClassMenu, false);
