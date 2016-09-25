9daedec55807ae93d225b46b7917460d0a922922
Eric Fennis <eric.fennis@gmail.com>
2016-09-25 18:17:16 +0200

    Aspoz requested changes are fixed, thanks for the feedback!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,49 +20,49 @@ closeMenu.addEventListener('click', toggleClassMenu, false);
 
 $(function() {
 
-var didScroll = null,
-    lastScrollTop = 0,
-    delta = 192,
-    headerTop = -96,
-    navbarHeight = $('.header.sticky').outerHeight();
+    var didScroll = null,
+        lastScrollTop = 0,
+        delta = 4,
+        headerTop = -96,
+        navbarHeight = $('.header.sticky').outerHeight();
 
-$(window).scroll(function(event){
-	didScroll = true;
+    $(window).scroll(function(event) {
+        didScroll = true;
 
-		if ($(document).scrollTop() <= headerTop) {
-	   		$('.header').removeClass('sticky');
-	  	} else {
-	    	$('.header').addClass('sticky');
-	  	}
-	});
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled($(this));
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled(scrollObj) {
-	
-    var st = scrollObj.scrollTop();
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta) {
-        return;
-    }
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight && $('.header').hasClass('sticky')){
-        // Scroll Down
-      console.log("add class");
-        $('.header.sticky').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.header.sticky').removeClass('nav-up').addClass('nav-down');
+        if ($(document).scrollTop() <= headerTop) {
+            $('.header').removeClass('sticky');
+        } else {
+            $('.header').addClass('sticky');
         }
-    }
-    
-    lastScrollTop = st;
-}
+    });
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled($(this));
+            didScroll = false;
+        }
+    }, 250);
 
-}		);
+    function hasScrolled(scrollObj) {
+
+        var st = scrollObj.scrollTop();
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta) {
+            return;
+        }
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight && $('.header').hasClass('sticky')) {
+            // Scroll Down
+            console.log("add class");
+            $('.header.sticky').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('.header.sticky').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+});
