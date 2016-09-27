3ecd455a9a87aad97103618ee93599093e52e13d
Sander Tuin <git@sander.garden>
2016-09-27 11:36:37 +0200

    Fix navbar to not show behind navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,49 @@
-function toggleClassMenu() {
-	myMenu.classList.add('menu--animatable');
-	if(!myMenu.classList.contains('menu--visible')) {
-		myMenu.classList.add('menu--visible');
-	} else {
-		myMenu.classList.remove('menu--visible');
+(function() {
+	function toggleClassMenu() {
+		myMenu.classList.add('menu--animatable');
+		if(!myMenu.classList.contains('menu--visible')) {
+			myMenu.classList.add('menu--visible');
+		} else {
+			myMenu.classList.remove('menu--visible');
+		}
 	}
-}
 
-function OnTransitionEnd() {
-	myMenu.classList.remove('menu--animatable');
-}
+	function OnTransitionEnd() {
+		myMenu.classList.remove('menu--animatable');
+	}
 
-var myMenu = document.querySelector('.menu');
-var oppMenu = document.querySelector('.menu-icon');
-var closeMenu = document.querySelector('.menu-close');
-myMenu.addEventListener('transitionend', OnTransitionEnd, false);
-oppMenu.addEventListener('click', toggleClassMenu, false);
-closeMenu.addEventListener('click', toggleClassMenu, false);
+	var myMenu = document.querySelector('.menu');
+	var oppMenu = document.querySelector('.menu-icon');
+	var closeMenu = document.querySelector('.menu-close');
+	myMenu.addEventListener('transitionend', OnTransitionEnd, false);
+	oppMenu.addEventListener('click', toggleClassMenu, false);
+	closeMenu.addEventListener('click', toggleClassMenu, false);
+})
 
-$(function() {
+$(function(){
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var $navbar = $('.header')
+	var navbarHeight = $navbar.outerHeight();
+	var $window = $(window);
 
-    var didScroll = null,
-        lastScrollTop = 0,
-        delta = 4,
-        headerTop = -96,
-        navbarHeight = $('.header.sticky').outerHeight();
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
 
-    $(window).scroll(function(event) {
-        didScroll = true;
+	setInterval(function() {
+		if (didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
 
-        if ($(document).scrollTop() <= headerTop) {
-            $('.header').removeClass('sticky');
-        } else {
-            $('.header').addClass('sticky');
-        }
-    });
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled($(this));
-            didScroll = false;
-        }
-    }, 250);
+	function hasScrolled() {
+		var st = $window.scrollTop();
 
-    function hasScrolled(scrollObj) {
-
+<<<<<<< 11f75c4ee94fd76eec14315a3f1cb0f43d44ef84
         var st = scrollObj.scrollTop();
         // Make sure they scroll more than delta
         if (Math.abs(lastScrollTop - st) <= delta) {
@@ -61,8 +61,24 @@ $(function() {
                 $('.header.sticky').removeClass('nav-up').addClass('nav-down');
             }
         }
+=======
+		// Make sure they scroll more than delta
+		if(Math.abs(lastScrollTop - st) <= delta)
+		return;
+>>>>>>> Fix navbar to not show behind navbar
 
-        lastScrollTop = st;
-    }
+		// If they scrolled down and are past the navbar, add class .nav-up.
+		// This is necessary so you never see what is "behind" the navbar.
+		if (st > lastScrollTop && st > navbarHeight){
+			// Scroll Down
+			$navbar.removeClass('nav-down').addClass('nav-up');
+		} else {
+			// Scroll Up
+			if(st + $window.height() < $(document).height()) {
+				$navbar.removeClass('nav-up').addClass('nav-down');
+			}
+		}
 
+		lastScrollTop = st;
+	}
 });
